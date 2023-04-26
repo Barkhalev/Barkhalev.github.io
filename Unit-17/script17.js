87c91cbbc00171c4259b786a181aa3eee3df2a56
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-04-27 02:24:02 +0500

    Типы данных JS Операторы присваивания Операторы сравнения Логические операторы Тернарный оператор, typeof  - узнать какой тип переменной создать переменные всех типов данных и вывести их в консоль. Также создать константу и объект.

--- Unit-17/script17.js
+++ Unit-17/script17.js
@@ -1,74 +1,98 @@
 // var colorPrimary = 'red';
-let colorSecondary = 'red';
+/*let colorSecondary = 'red';
 const colorBase = 'gold';
 
 console.log(colorSecondary);
 console.log(colorBase);
+*/
 
 // Типы данных JS
 
-let numVar = 23; //number
+/*let numVar = 23; //number
 let strVar = 'Любой строки'; // string
-let booVar = true; // boolean true false
+let booVar = true; // boolean true(истина/правда) false(ложь)
 let undefVar = undefined; //Undefined - значение еще небыло сщздано
 let nullVar = null; //Null - создали но не назначили значение
+let objVar = {} //object - коллекции значений
+*/
 
-let objVar = {
-    //переменная 1 colorSecondary = 'red';
-    //переменная 2 colorBase = 'red';
-} //object - коллекции значений
+//Операторы присваивания
+/*
+Математические
+Сложение +, можем складывать строки
+Вычитание - 
+Умножение *
+деление / 
+Взятие остатка от деления %
+Возведение в степень **.
+*/
+let var1 = 23;
+let var2 = 3;
+console.log((var1 - 2 )* var2);
 
-// user = {
-//     name: 'Max',
-//     age:'31'
-// }
-// console.log(user.name,user.age);
+// Операторы сравнения
+/*
+Равно (==)
+Не равно (!=)
+Строго равно (===)
+Строго не равно (!==)
+Больше(>)
+Больше или равно (>=)
+Меньше(<)
+Меньше или равно(<=)
+*/
+var1 = 3;
+var2 = 7;
+console.log(var1 == var2);
+console.log(var1 !== var2);
+console.log(var1 >= var2);
 
-let string = 'Hello' + ' world!';
-console.log(string);
+// Логические операторы
+/*
+Логическое И -(&&)
+Логическое ИЛИ -(||)
+Логическое НЕ -(!)
+*/
+var1 = 5;
+var2 = 7;
+console.log(var1 > var2);
+console.log(true && true);
+console.log(false || true);
+console.log(!false);
+console.log(!true);
 
-// let val1 = '4'; //string
-// let val2 = '5'; //string
-// let sum = val1 + val2;
-// console.log(sum);
+const user = {
+    age: 23
+}
 
-let val1 = 4; //number
-let val2 = 5; //number
-let sum = val1 + val2;
-console.log(sum);
+if (!(user.age > 18)){
+    console.log("Совершеннолетний");
+}else {
+    console.log("Не совершеннолетний");
+}
+if (user.age > 18){
+    console.log("Совершеннолетний");
+}else {
+    console.log("Не совершеннолетний");
+}
 
+if (true){
+    console.log("Совершеннолетний");
+}else {
+    console.log("Не совершеннолетний");
+}
+if (!true){
+    console.log("Совершеннолетний");
+}else {
+    console.log("Не совершеннолетний");
+}
 
-// let obj2 = new Object();
-// let obj = {};
-let user = {
-    name: 'Max',
-    age: 37,
-    status: {
-        admin: true,
-        privace: false
-    }
-};
-console.log('До создания возраста', user);
+//Тернарный оператор
+// логическое значение ? вернется если истина : вернется если ложь
+let age = 19;
+let teens = age <= 18 ? false : true;
+console.log("Совершеннолетний", teens);
 
-setTimeout(() => {
-user.age = 31;
-
-console.log('После создания возраста', user.name, user.age + 1, user.status);
-},2500)
-
-setTimeout(() => {
-    delete user.age;
-
-    console.log('После удаления возраста', user.name, user.age + 1, user.status);
-},5000)
-    
-
-
-const user1 = {
-    name: 'Max',
-    status: {
-        admin: true,
-        privace: false
-    }
-};
-console.log('До создания возраста', user1);
+// typeof  - узнать какой тип переменной 
+console.log(typeof age);
+console.log(typeof teens);
