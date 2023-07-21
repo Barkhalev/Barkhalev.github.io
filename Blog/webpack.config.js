901aa142da40c00ec85bf5d779cfdea8898e3491
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-07-21 16:48:35 +0500

    modified .gitignore

--- Blog/webpack.config.js
+++ Blog/webpack.config.js
@@ -2,39 +2,42 @@ const HTMLPlugin = require('html-webpack-plugin')
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: ['./src/index.js'],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/dist'),
     },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '/dist'),
-          },
-        // contentBase: __dirname + '/dist',
-        compress: true,
-        // port: 9000,
-    },
-    plugins: [
-        new HTMLPlugin({
-            template: './src/index.html', 
-            filename: 'index.html'                    
-        }),
-    ],
-    resolve: {
-        extensions: ['.js']
-    }
-};
-
-module: {
+    // contentBase: __dirname + '/dist',
+    compress: true,
+    // port: 9000,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+  ],
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-      },
+      }
     ]
-}
+  }
+
+};
