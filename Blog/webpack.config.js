6970563bd832a281c0ccfedfe2cd1d5ed814c55c
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-07-21 15:09:24 +0500

    Add node_modules/webpack, add module.exports, devServer

--- Blog/webpack.config.js
+++ Blog/webpack.config.js
@@ -1,8 +1,40 @@
+const HTMLPlugin = require('html-webpack-plugin')
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: ['./src/index.js'],
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/dist'),
+          },
+        // contentBase: __dirname + '/dist',
+        compress: true,
+        // port: 9000,
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: './src/index.html', 
+            filename: 'index.html'                    
+        }),
+    ],
+    resolve: {
+        extensions: ['.js']
     }
 };
+
+module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+}
