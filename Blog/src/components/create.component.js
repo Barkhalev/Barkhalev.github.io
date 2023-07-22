342d3a869ffe7d32fb587dd5f95f00d793ea6322
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-07-22 17:10:41 +0500

    Создание записи в Firebase

--- Blog/src/components/create.component.js
+++ Blog/src/components/create.component.js
@@ -1,16 +1,17 @@
-import {Component} from '../core/component';
+import { Component } from '../core/component';
 import { Form } from '../core/form';
 import { Validators } from '../core/validators';
+import { apiService } from '../services/api.service';
 
 export class CreateComponent extends Component {
-    constructor (id) {
+    constructor(id) {
         super(id)
     }
 
     init() {
         this.$el.addEventListener('submit', submitHandler.bind(this))
 
-        
+
         this.form = new Form(this.$el, {
             title: [Validators.required],
             fulltext: [Validators.required, Validators.minLength(10)]
@@ -18,16 +19,18 @@ export class CreateComponent extends Component {
     }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
     event.preventDefault()
 
     if (this.form.isValid()) {
-         const formData = {
-        type: this.$el.type.value,
-        ...this.form.value()
-    }
-    this.form.clear()
+        const formData = {
+            type: this.$el.type.value,
+            date: new Date().toLocaleDateString(),
+            ...this.form.value()
+        }
 
-    console.log('Submit', formData)   
+        await apiService.createPost(formData)
+        this.form.clear()
+        alert('Запись создана в базе данных')
     }
 }
