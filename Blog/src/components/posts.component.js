0be79b0f26892bf133c17240e8645c504f638df7
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-07-28 15:16:33 +0500

    Сделал кнопку сохранить, удалить с фиксацией в консоли на посещение

--- Blog/src/components/posts.component.js
+++ Blog/src/components/posts.component.js
@@ -8,6 +8,10 @@ export class PostsComponent extends Component {
         this.loader = loader
     }
 
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this))
+    }
+
     async onShow() {
         this.loader.show()
         const fbData = await apiService.fetchPosts()
@@ -27,7 +31,11 @@ function renderPost(post) {
         ? '<li class="tag tag-blue tag-rounded">Новость</li>'
         : '<li class="tag tag-rounded">Заметка</li>'
 
-    const button = '<button class="buttton-round button-small button-primary">Сохранить</button>'
+    const button = (JSON.parse(localStorage.getItem('favorites')) || [] ).includes(post.id)
+        ? `<button class="buttton-round button-small button-danger"
+        data-id="${post.id}">Удалить</button>`
+        : `<button class="buttton-round button-small button-primary"
+        data-id="${post.id}">Сохранить</button>`
 
     return `
         <div class="panel">
@@ -46,4 +54,30 @@ function renderPost(post) {
             </div>
         </div>
     `
+}
+
+function buttonHandler(event) {
+    const $el = event.target
+    const id = $el.dataset.id
+
+    if (id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) ||  []
+        
+        if (favorites.includes(id)) {
+            //удалить элемент
+            $el.textContent = 'Сохранить'
+            $el.classList.add('button-primary')
+            $el.classList.remove('button-danger')
+
+            favorites = favorites.filter(fId => fId !== id )
+        }else {
+            //добавить элемент
+            $el.classList.remove('button-primary')
+            $el.classList.add('button-danger')
+            $el.textContent = 'Удалить'
+            favorites.push(id)
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }
 }
