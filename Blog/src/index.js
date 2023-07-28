9db2bda9c7f0125cbe6b3e036b6dcc29abd40bae
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-07-28 14:05:12 +0500

    Добавил индикатор загрузки loader

--- Blog/src/index.js
+++ Blog/src/index.js
@@ -1,19 +1,21 @@
-import {HeaderComponent} from './components/header.component'
-import {NavigationComponent} from './components/navigationcomponent'
+import { HeaderComponent } from './components/header.component'
+import { NavigationComponent } from './components/navigationcomponent'
 import { CreateComponent } from './components/create.component'
 import { FavoriteComponent } from './components/favorite.component'
 import { PostsComponent } from './components/posts.component'
+import { LoaderComponent } from './components/loader.component'
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+const loader = new LoaderComponent('loader')
 
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', {loader})
 const create = new CreateComponent('create')
 const favorite = new FavoriteComponent('favorite')
 
 navigation.registerTabs([
-    {name: 'create', component: create},
-    {name: 'posts', component: posts},
-    {name: 'favorite', component: favorite}
+    { name: 'create', component: create },
+    { name: 'posts', component: posts },
+    { name: 'favorite', component: favorite }
 ])
