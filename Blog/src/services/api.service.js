0cfd53700c90d0d2954ebaa451776811acc552cd
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-09-20 18:54:20 +0500

    Изменил https://blog-js-d499b-default-rtdb.firebaseio.com

--- Blog/src/services/api.service.js
+++ Blog/src/services/api.service.js
@@ -43,4 +43,4 @@ async function useRequest(request) {
     return await response.json()
 }
 
-export const apiService = new ApiService('https://blog-js-d499b-default-rtdb.firebaseio.comgi')
+export const apiService = new ApiService('https://blog-js-d499b-default-rtdb.firebaseio.com')
